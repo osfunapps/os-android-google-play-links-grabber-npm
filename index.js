9fbb45b21c416e680a9a31e1c86b19ad9decd6d1
osfunapps <osfunapps@gmail.com>
2021-02-07 16:29:31 +0200

    fixed readme

--- index.js
+++ index.js
@@ -1,30 +1,44 @@
+const ph = require('os-puppeteer-helper');
+
 const self = module.exports = {
 
     /**
      * Will grab all of the google play links from a given Google Play Console account.
      *
      * @param page -> an optional puppeteer page
+     * @param filterSuspendedRemovedAndRejectedApps -> toggle true to remove suspended, removed or
+     * rejected apps from the list
+     * @param fireFoxNightlyPath the path to your FireFox Nightly runner file.
+     * In Windows that's usually your firefox.exe file (like 'C:/Program Files/Firefox Nightly/firefox.exe').
+     * In Mac that's usually your firefox file, located in your Firefox Nightly.app, inside the Applications dir.
      * @return -> a list of dictionary values. Carrying the app name, the package name and the app link.
      */
-    grabGooglePlayLinks: async function(page=null) {
-        if(page === null) {
-            let pEles = await ph.createBrowser()
+    grabGooglePlayLinks: async function (page = null,
+                                         filterSuspendedRemovedAndRejectedApps = true,
+                                         fireFoxNightlyPath = '/Applications/Firefox Nightly.app/Contents/MacOS/firefox') {
+        if (page === null) {
+            let pEles = await ph.createFirefoxBrowser("about:blank",
+                5,
+                false,
+                1300,
+                768,
+                fireFoxNightlyPath)
             page = pEles[1]
         }
         await ph.navigateTo(page, 'https://play.google.com/apps/publish/')
-        return await grabLinksFormNextPage(page, [])
+        return await grabLinksFormNextPage(page, filterSuspendedRemovedAndRejectedApps, [])
     }
 };
 
-async function grabLinksFormNextPage(page, playLinks) {
-    playLinks = await grabLinksFromPage(page, playLinks)
+async function grabLinksFormNextPage(page, filterSuspendedRemovedAndRejectedApps, playLinks) {
+    playLinks = await grabLinksFromPage(page, filterSuspendedRemovedAndRejectedApps, playLinks)
 
     let nextPageBtn = await ph.getElement(page, "button[aria-label='Next page']")
-    if(nextPageBtn !== undefined) {
+    if (nextPageBtn !== undefined) {
         let nextBtnDisabled = await ph.isElementHasAttribute(page, nextPageBtn, 'disabled')
-        if(!nextBtnDisabled) {
+        if (!nextBtnDisabled) {
             await ph.clickOnElement(page, nextPageBtn, 3000, "button[aria-label='Previous page']")
-            playLinks = await grabLinksFormNextPage(page, playLinks)
+            playLinks = await grabLinksFormNextPage(page, filterSuspendedRemovedAndRejectedApps, playLinks)
         }
     }
 
@@ -32,7 +46,7 @@ async function grabLinksFormNextPage(page, playLinks) {
 }
 
 
-async function grabLinksFromPage(page, playLinks) {
+async function grabLinksFromPage(page, filterSuspendedRemovedAndRejectedApps, playLinks) {
     let APP_INDICATION = 'Application '
     let PACKAGE_NAME_INDICATION = ', package name '
     await ph.waitForSelector(page, "div[__gwt_cell] img")
@@ -45,6 +59,11 @@ async function grabLinksFromPage(page, playLinks) {
         let packageName = ariaLabel.substring(ariaLabel.indexOf(PACKAGE_NAME_INDICATION) + PACKAGE_NAME_INDICATION.length)
         let currUrl = await page.url()
         let appRawLink = await ph.getAttributeValueFromElement(page, aEle, 'href')
+        let stausAs = await ph.getElements(page, "a[href='" + appRawLink + "']")
+        let appStatus = await ph.getAttributeValueFromElement(page, stausAs[3], 'aria-label')
+        if (appStatus !== 'App status is Published' && filterSuspendedRemovedAndRejectedApps) {
+            continue
+        }
         let appLink = currUrl.substring(0, currUrl.indexOf('#AppListPlace')) + appRawLink
         playLinks.push({
             appName: appName,
